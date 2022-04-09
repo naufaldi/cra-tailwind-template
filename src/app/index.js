350cff6a2dd3b475cced081823eb073aba7316af
Toton Dwi Antoko <Totonantoko@gmail.com>
2022-04-09 14:02:56 +0700

    change css to scss

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import logo from 'assets/logo.svg'
-import 'styles/App.css'
+import 'styles/App.scss'
 
 function App () {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'styles/index.css'
+import 'styles/index.scss'
 import App from 'app'
 import reportWebVitals from 'reportWebVitals'
 
